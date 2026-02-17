7b1b71da7faf1808a68794b7b78c5a96ac0fe084
Federico Santoro <f.santoro2008gmail.com>
2026-02-17 13:49:15 +0000

    basi di js

---
+++ es03a.js
@@ -0,0 +1,5 @@
+let lunghezza = prompt("Inserisci la lunghezza: ");
+let altezza = prompt("Inserisci l'altezza");
+let larghezza = prompt("Inserisci la larghezza");
+
+console.log("Volume: " + lunghezza*altezza*larghezza);
