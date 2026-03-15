dc50d9e093a7d8fa00b7e4e5ca87e6b6d185fa95
Federico Santoro <f.santoro2008gmail.com>
2026-03-15 17:16:48 +0000

    Aggiunto design moderno e menu dropdown

---
+++ es04/es04a4.js
@@ -0,0 +1,28 @@
+const args = process.argv.slice(2);
+
+  if (args.length === 0) {
+    console.log("ERRORE! Inserisci num operazione num");
+    console.log("Esempio: node calc.js 5 som 3");
+    console.log("operazione valide: Somma: som, Sottrazione , sot, Moltiplicazione: molt, Divisione: div");
+    process.exit(1); // per terminare il programma
+  }
+
+  let num1 = Number(process.argv[2]);
+  let operazione = process.argv[3];
+  let num2 = Number(process.argv[4]);
+
+  if (operazione == "som"){
+    console.log("Risultato: " ,num1+num2);
+  }else if (operazione == "sot"){
+    console.log("Risultato: " ,num1-num2);
+  }else if (operazione == "molt"){
+    console.log("Risultato: " ,num1*num2);
+  }else if (operazione == "div"){
+    if (num2 === 0) {
+        console.log("Errore: Impossibile dividere per zero!");
+    } else {
+        console.log("Risultato:", num1 / num2);
+    }
+  }else{
+    console.log("ERRORE! Operazione inserita non valida");
+  }
